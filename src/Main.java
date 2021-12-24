d37e5057f77533a6fb0e6fcc6f81eb835562d70c
IvanNetology <ivan.ivan.91.91@internet.ru>
2021-12-24 13:00:25 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1100;
+        int money = 20;
+        int miles = price / money;
+        System.out.println("Количество начисленных миль - " + miles);
+    }
+}
